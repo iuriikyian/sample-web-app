e2c79073bae9183f7f9d9a8db661fc60b66c8c8e
Iurii Kyian <iuriikyian@Iuriis-MacBook-Pro.local>
2016-12-03 13:14:10 +0100

    added replicaset sample config

--- app-oauth2/config/database.js
+++ app-oauth2/config/database.js
@@ -2,9 +2,19 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+var dbUrl = 'mongodb://localhost:27017/localAuthTest';
+
+var replicaSetUrl = 'mongodb://localhost:27020,localhost:27021,localhost:27022/replicasetTest?replicaSet=replica';
+var replicaSetOptions = {
+    db: { native_parser: true },
+    server: { poolSize: 5 },
+    replset: { rs_name: 'replica' }
+};
+
 module.exports = {
     connect : function(){
-        mongoose.connect('mongodb://localhost:27017/localAuthTest');
+        mongoose.connect(dbUrl);
+        // mongoose.connect(replicaSetUrl, replicaSetOptions);
     },
     connection : mongoose.connection
 };

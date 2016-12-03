47687d043216751210ad1f45419e3dea2e404a02
Iurii Kyian <iuriikyian@Iuriis-MacBook-Pro.local>
2016-12-04 00:08:09 +0100

    configured aliases for replica set servers

--- app-oauth2/config/database.js
+++ app-oauth2/config/database.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 var dbUrl = 'mongodb://localhost:27017/localAuthTest';
 
 // var replicaSetUrl = 'mongodb://localhost:27021,localhost:27022,localhost:27023/replicasetTest?replicaSet=replica';
-var replicaSetUrl = 'mongodb://10.0.0.26:27017,10.0.0.55:27017,10.0.0.56:27017/replicasetTest?replicaSet=replica';
+var replicaSetUrl = 'mongodb://srv1.web.com:27017,srv2.web.com:27017,srv3.web.com:27017/replicasetTest?replicaSet=replica';
 var replicaSetOptions = {
     db: { native_parser: true },
     server: { poolSize: 5 },
